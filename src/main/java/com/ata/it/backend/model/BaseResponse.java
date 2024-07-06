bbaa0b4122cc4758bda913f16faba61f9a7b7109
ginying <>
2024-07-06 12:35:16 +0700

    Add global error handler for REST API

---
+++ src/main/java/com/ata/it/backend/model/BaseResponse.java
@@ -0,0 +1,13 @@
+package com.ata.it.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseResponse {
+    private String statusCode;
+    private String message;
+}
---
+++ src/main/java/com/ata/it/backend/rest/GlobalExceptionRestController.java
@@ -0,0 +1,21 @@
+package com.ata.it.backend.rest;
+
+import com.ata.it.backend.model.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionRestController  extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<BaseResponse> exceptionHandler(Exception exception){
+        BaseResponse response = new BaseResponse("500", exception.getMessage());
+        return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
